20edf9988a0cfa0a033997b34422f39f4b760970
Edvinas <edvinas.sniuolis@gmail.com>
2018-03-06 22:55:38 +0200

    Init Commit

---
+++ ADS_Recursion_11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADS_Recursion_11.Service;
+
+namespace ADS_Recursion_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = NumberGenerator.Random(10, -10, 10);
+
+            foreach (var i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ADS_Recursion_11/Service/NumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADS_Recursion_11.Service
+{
+    static class NumberGenerator
+    {
+        public static List<int> Random(int size, int minValue, int maxValue)
+        {
+            var random = new Random();
+            var randomList = new List<int>();
+
+            for (int i = 0; i < size; i++)
+                randomList.Add(random.Next(minValue, maxValue));
+
+            return randomList;
+        }
+    }
+}
